9cd178fcbb0e9ea4ea47cd7c04ef87d9730f54dc
Kyle Berry <berrytechnics@gmail.com>
2023-04-04 21:57:46 -0500

    add usercontroller

---
+++ controllers/UserController.js
@@ -0,0 +1,7 @@
+import User from "../models/User.js";
+
+export const login = (req, res, next) => {res.send(200)};
+
+export const register = (req, res, next) => {res.send(200)};
+
+export const dashboard = (req, res, next) => {res.send(200)};
