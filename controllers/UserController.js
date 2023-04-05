5b8b46ae0a1ed5c4e3c5392f3d104799847d2c4a
Kyle Berry <berrytechnics@gmail.com>
2023-04-04 22:49:36 -0500

    user system improvemenets

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,38 @@
 import User from "../models/User.js";
 
-export const login = (req, res, next) => {res.send(200)};
+export const login = async (req, res, next) => {
+  if (req.method === "POST") {
+    try {
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  }
+  res.render("admin/login", { model: [] });
+};
 
-export const register = (req, res, next) => {res.send(200)};
+export const register = async (req, res, next) => {
+  if (req.method === "POST") {
+    try {
+      User.register(
+        { username: req.body.username },
+        req.body.password,
+        (err) => {
+          next(err);
+        }
+      );
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  }
+  res.render("admin/register", { model: [] });
+};
 
-export const dashboard = (req, res, next) => {res.send(200)};
+export const dashboard = async (req, res, next) => {
+  try {
+    res.render("admin/dashboard", { data: [] });
+  } catch (err) {
+    next(err);
+  }
+};
