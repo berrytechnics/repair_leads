c82782c5b9839e31e31f885ca4787d57c49e0cfc
Kyle Berry <berrytechnics@gmail.com>
2023-04-04 21:57:15 -0500

    routes

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
+import * as User from '../controllers/UserController.js';
 import * as Pricelist from '../controllers/PricelistController.js';
 
 const router = express.Router();
 
-// import * as svgCaptcha from "svg-captcha";
+router.all('/login', User.login );
+router.all('/register', User.register );
+router.all('/dashboard', User.dashboard );
 
-router.get('/pricelist', Pricelist.index )
+router.all('/pricelist', Pricelist.index );
+router.all('/pricelist/create', Pricelist.create );
+router.all('/pricelist/view/:id', Pricelist.view );
+router.all('/pricelist/update/:id', Pricelist.update);
 
 export default router;
